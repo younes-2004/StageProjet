86addc107f4054472893b85e89bff2042a478807
firdaousse13 <flowerfitdaousse@gmail.com>
2025-06-11 10:12:20 +0100

    finalisation du projet1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'إدارة الملفات القضائية') }}</title>
+     <link rel="icon" type="image/png" href="{{ asset('image1.png') }}">
     
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
